8d4c9d8fd3eebc058f11e0e1a1c8d685d97b9380
Erick Nunes <erickFernandes000@gmail.com>
2025-03-15 19:04:03 -0300

    adicionando novo construtor a classe de erro ParametrosInvalidosException

--- src/main/java/com/board/cardboard/contador/handler/ParametrosInvalidosException.java
+++ src/main/java/com/board/cardboard/contador/handler/ParametrosInvalidosException.java
@@ -2,7 +2,11 @@ package com.board.cardboard.contador.handler;
 
 public class ParametrosInvalidosException extends Exception{
 
-    public ParametrosInvalidosException(){
+    public ParametrosInvalidosException(){//mensagem de erro padrão
         super("O segundo parâmetro deve ser maior que o primeiro");
+    }
+
+    public ParametrosInvalidosException(String message){//criando personalização de messagem de erro
+        super(message);
     }
 }
